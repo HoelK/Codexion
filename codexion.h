8285afb2a0109e6c1bba6ba2e7920d3fb1a5555e
HoelK <KeromnesHoel@gmail.com>
2026-03-22 06:43:18 +0100

    established main structure

--- codexion.h
+++ codexion.h
@@ -1,13 +1,84 @@
+# pragma once
 # include <stdio.h>
+# include <stdint.h>
+# include <stddef.h>
 # include <stdlib.h>
 # include <limits.h>
 # include <unistd.h>
 # include <string.h>
 # include <stdbool.h>
-# define AC_REQUIRED 9
+# include <pthread.h>
+# include <sys/time.h>
+# define AC_REQUIRED	9
+# define AV_NB_CODERS	av[1]
+# define AV_TBURNOUT	av[2]
+# define AV_TCOMPILE	av[3]
+# define AV_TDEBUG		av[4]
+# define AV_TREFACT		av[5]
+# define AV_NCOMPILE	av[6]
+# define AV_CDDONGLE	av[7]
+# define RIGHT			0
+# define LEFT			1
+# define DONGLE_MESSAGE		"has taken a dongle\n"
+# define COMPILE_MESSAGE	"is compiling\n"
+# define DEBUG_MESSAGE		"is debugging\n"
+# define REFACTOR_MESSAGE	"is refactoring\n"
+# define BURNOUT_MESSAGE	"burned out\n"
 
-int		ft_isdigit(int c);
-bool	ft_isspace(char c);
-long	ft_atol(const char *str);
+enum e_status
+{
+	DONGLE,
+	COMPILE,
+	DEBUG,
+	REFACTOR,
+	BURN_OUT
+};
+
+typedef struct s_hub t_hub;
+typedef struct s_coder t_coder;
+
+typedef struct s_dongle
+{
+	int				queue[2];
+	int				q_size;
+	t_coder			*coders[2];
+	pthread_mutex_t	mutex;
+}	t_dongle;
+
+typedef struct s_coder
+{
+	int			id;
+	bool		burnout;
+	pthread_t	thread;
+	t_dongle	*dongle[2];
+	t_hub		*hub;
+}	t_coder;
+
+typedef struct s_hub
+{
+	uint32_t	start;
+	int			n_coders;
+	int			n_compiles;
+	int			burnout_time;
+	int			compile_time;
+	int			debug_time;
+	int			refactor_time;
+	int			dongle_cd;
+	t_coder		*coders;
+	t_dongle	*dongles;
+}	t_hub;
+
+int			ft_isdigit(const char c);
+bool		ft_isspace(const char c);
+long		ft_atol(const char *str);
+void		*ft_bzero(void *s, size_t n);
+
+void		print_status(int id, uint32_t start, uint8_t status);
+
+uint32_t	timestamp(void);
+void		ft_sleep(uint32_t u_time);
+
+void	free_hub(t_hub *hub);
+
+bool	init(t_hub *hub, char **av);
 bool	check_args(int ac, char **av);
-int		ft_strcmp(const char *s1, const char *s2);
--- main.c
+++ main.c
@@ -1,11 +1,49 @@
 # include "codexion.h"
 
+//queue system : dongles have 2 pointers(left and right coder)
+//they also have an array of 2 integers associated with a stack size, the value of integers can be either RIGHT, LEFT or EMPTY, but they cannot have the same value (except for empty)
+
+static void	*test(void *data)
+{
+	int		caca;
+	t_coder	*coder = (t_coder *) data;
+	
+	caca = 1;
+	caca++;
+	caca++;
+	caca++;
+	printf("[time]%d\n", timestamp() - coder->hub->start);
+	pthread_mutex_lock(&coder->dongle[RIGHT]->mutex);
+	print_status(coder->id, coder->hub->start, DONGLE);
+	pthread_mutex_lock(&coder->dongle[LEFT]->mutex);
+	print_status(coder->id, coder->hub->start, DONGLE);
+	printf("ok\n");
+	pthread_mutex_unlock(&coder->dongle[RIGHT]->mutex);
+	pthread_mutex_unlock(&coder->dongle[LEFT]->mutex);
+
+	return (coder);
+}
+
+static void start(t_hub *hub)
+{
+	hub->start = timestamp();
+	for (int i = 0; i < hub->n_coders; i++)
+		pthread_create(&hub->coders[i].thread, NULL, &test, &hub->coders[i]);
+	for (int i = 0; i < hub->n_coders; i++)
+		pthread_join(hub->coders[i].thread, NULL);
+}
+
 int main(int ac, char **av)
 {
-	if (!check_args(ac, av))
+	t_hub	hub;
+
+	ft_bzero(&hub, sizeof(t_hub));
+	if (!check_args(ac, av) || !init(&hub, av))
 	{
 		write(2, "Error\n", 6);
-		return (EXIT_FAILURE);
+		return (free_hub(&hub), EXIT_FAILURE);
 	}
+	start(&hub);
+	free_hub(&hub);
 	return (EXIT_SUCCESS);
 }
